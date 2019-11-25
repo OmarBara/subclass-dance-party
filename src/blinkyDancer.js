8eefef9a5ea051d2b746784a1567d08f240b940f
omarbara <omarbara@ymail.com>
2019-11-25 20:35:41 +0100

    3sprint-day1

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,5 +1,6 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+  //makeDancer.call(this);
+  var blinkyDancer = new makeDancer(top, left, timeBetweenSteps);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
@@ -17,3 +18,4 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
 
   return blinkyDancer;
 };
+
